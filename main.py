8024a3cb4a821e67648a43936f785d72116e562e
Thalaivar <dhruvlaad@gmail.com>
2020-10-15 22:37:08 +0530

    evaluation added

--- main.py
+++ main.py
@@ -22,7 +22,8 @@ transform_test = transforms.Compose([
                     ])
 
 def train(data_dir: str):
-    update_freq = 1000
+    update_freq = 100
+    eval_freq = 5000
     wandb.init(project='mini-assignment-3')
 
     model = Network(in_channels=3)
@@ -44,13 +45,15 @@ def train(data_dir: str):
     wandb.config.lr = lr
 
     train_data = datasets.CIFAR100(root=data_dir, train=True, transform=transform_train, download=True)
-    dataloader = DataLoader(train_data, batch_size, shuffle=True, num_workers=4)
+    train_dataloader = DataLoader(train_data, batch_size, shuffle=True, num_workers=4)
+    test_data = datasets.CIFAR100(root=data_dir, train=False, transform=transform_test, download=True)
+    test_dataloader = DataLoader(test_data, batch_size, shuffle=True, num_workers=4)
 
-    logging.info(f'Beginning training for {n_epochs} epochs ({n_epochs*len(dataloader)*batch_size} steps) on device: {device}')
+    logging.info(f'Beginning training for {n_epochs} epochs ({n_epochs*len(train_dataloader)*batch_size} steps) on device: {device}')
     steps = 0
     running_loss = 0.0
     for _ in range(n_epochs):
-        for i, data in enumerate(dataloader, 0):
+        for i, data in enumerate(train_dataloader, 0):
             inputs, labels = data
             inputs, labels = inputs.to(device), labels.to(device)
 
@@ -69,6 +72,29 @@ def train(data_dir: str):
                 wandb.log({'loss': running_loss/update_freq}, step=steps)
                 logging.info(f'Steps: {steps} ; Loss: {running_loss/update_freq}')
                 running_loss = 0.0
+
+            if steps % eval_freq == 0:
+                eval_acc = evaluate_model(model, test_dataloader)
+                wandb.log({'eval accuracy': eval_acc}, step=steps)
         
     torch.save(model.parameters(), './final_model.wts')
-    wandb.save("model.h5")
+    wandb.save("final_model.h5")
+
+def evaluate_model(model, dataloader):
+    model.eval()
+
+    total, correct = 0, 0
+    with torch.no_grad():
+        for data in dataloader:
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.cpu().data, 1)
+
+            total += labels.shape[0]
+            correct += (predicted == labels).sum().item()
+
+    logging.info(f'evaluation accuracy: {round(100 * correct // total, 2)}%')    
+            
+    return correct / total
