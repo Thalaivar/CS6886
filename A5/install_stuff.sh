2e304efeb667594b8d55f3dae3d28fa8c3d1e936
Thalaivar <dhruvlaad@gmail.com>
2020-12-14 12:44:08 +0530

    make models dir and download there

--- A5/install_stuff.sh
+++ A5/install_stuff.sh
@@ -18,7 +18,8 @@ sudo apt-get install libcudnn8-dev=8.0.5.39-1+cuda10.2
 
 sudo apt autoremove -y
 
-mkdir ~/models && cd ~/models
+mkdir ~/models
+cd ~/models
 wget https://zenodo.org/record/2592612/files/resnet50_v1.onnx
 wget https://zenodo.org/record/3157894/files/mobilenet_v1_1.0_224.onnx
 wget https://zenodo.org/record/3163026/files/ssd_mobilenet_v1_coco_2018_01_28.onnx
@@ -26,5 +27,5 @@ wget https://zenodo.org/record/3228411/files/resnet34-ssd1200.onnx
 
 
 conda install pytorch torchvision torchaudio cudatoolkit=10.2 -c pytorch -y 
-pip install fastBPE sacremoses subword_nmt omegaconf requests opencv-python onnx onnxruntime-gpu
+pip install opencv-python onnx onnxruntime-gpu
 pip install fairseq==0.10.0
