e2c14f4711c9e9548714c819a458582749afb898
Thalaivar <dhruvlaad@gmail.com>
2020-12-18 15:03:29 +0530

    ipython version

--- project/main.py
+++ project/main.py
@@ -15,7 +15,7 @@ def get_model_info(model, name):
     model.eval()
     model.to('cuda:0')
 
-    wandb.init(project='sysdl-term-project', name=f'{name}-inference')
+    wandb.init(project='sysdl-term-project', name=f'{name}-inference' )
     wandb.watch(model)
 
     total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -43,42 +43,52 @@ def get_model_info(model, name):
     
     return model_info
 
-# nasnet = NASNetALarge()
-# model_info = get_model_info(nasnet, 'NASNet-A Large')
-# print(model_info)
+def get_NASNetALarge_info():
+    nasnet = NASNetALarge()
+    model_info = get_model_info(nasnet, 'NASNet-A Large')
+    print(model_info)
 
-# FBNetv2 = fbnet('dmasking_f4')
-# model_info = get_model_info(FBNetv2, name='FBNet-v2')
-# print(model_info)
+def get_FBNetV2_info():
+    FBNetv2 = fbnet('dmasking_f4')
+    model_info = get_model_info(FBNetv2, name='FBNet-v2')
+    print(model_info)
 
-# mnasnet = MnasNet()
-# model_info = get_model_info(mnasnet, name='MNasNet')
-# print(model_info)
+def get_MNasNet_info():
+    mnasnet = MnasNet()
+    model_info = get_model_info(mnasnet, name='MNasNet')
+    print(model_info)
 
-# inceptionv3 = torch.hub.load('pytorch/vision:v0.6.0', 'inception_v3', pretrained=False)
-# model_info = get_model_info(inceptionv3, name='Inception-v3')
-# print(model_info)
+def get_InceptionV3_info():
+    inceptionv3 = torch.hub.load('pytorch/vision:v0.6.0', 'inception_v3', pretrained=False)
+    model_info = get_model_info(inceptionv3, name='Inception-v3')
+    print(model_info)
 
-# efficientnetb7 = EfficientNet.from_name('efficientnet-b7')
-# model_info = get_model_info(efficientnetb7, name='EfficientNetB7')
-# print(model_info)
+def get_EfficientNetB7_info():
+    efficientnetb7 = EfficientNet.from_name('efficientnet-b7')
+    model_info = get_model_info(efficientnetb7, name='EfficientNetB7')
+    print(model_info)
 
-# efficientnetb0 = EfficientNet.from_name('efficientnet-b0')
-# model_info = get_model_info(efficientnetb0, name='EfficientNetB0')
-# print(model_info)
+def get_EfficientNetB0_info():
+    efficientnetb0 = EfficientNet.from_name('efficientnet-b0')
+    model_info = get_model_info(efficientnetb0, name='EfficientNetB0')
+    print(model_info)
 
-# resnext_101 = torch.hub.load('pytorch/vision:v0.6.0', 'resnext101_32x8d', pretrained=False)
-# model_info = get_model_info(resnext_101, name='ResNeXt-101')
-# print(model_info)
+def get_ResNeXt101_info():
+    resnext_101 = torch.hub.load('pytorch/vision:v0.6.0', 'resnext101_32x8d', pretrained=False)
+    model_info = get_model_info(resnext_101, name='ResNeXt-101')
+    print(model_info)
 
-# mobilenet_v2 = torch.hub.load('pytorch/vision:v0.6.0', 'mobilenet_v2', pretrained=True)
-# model_info = get_model_info(mobilenet_v2, name='MobileNet-V2')
-# print(model_info)
+def get_MobileNetV2_info():
+    mobilenet_v2 = torch.hub.load('pytorch/vision:v0.6.0', 'mobilenet_v2', pretrained=True)
+    model_info = get_model_info(mobilenet_v2, name='MobileNet-V2')
+    print(model_info)
 
-# mobilenet_v3 = MobileNetV3(mode='large')
-# model_info = get_model_info(mobilenet_v3, name='MobileNetV3')
-# print(model_info)
+def get_MobileNetV3_info():
+    mobilenet_v3 = MobileNetV3(mode='large')
+    model_info = get_model_info(mobilenet_v3, name='MobileNetV3')
+    print(model_info)
 
-# resent50 = torch.hub.load('pytorch/vision:v0.6.0', 'resnet50', pretrained=False)
-# model_info = get_model_info(resent50, 'ResNet-50')
-# print(model_info)
+def get_ResNet50_info():
+    resent50 = torch.hub.load('pytorch/vision:v0.6.0', 'resnet50', pretrained=False)
+    model_info = get_model_info(resent50, 'ResNet-50')
+    print(model_info)
