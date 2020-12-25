46429f14b516a5f108df6ff0bf265770f7ccc039
Dhruv Laad <dhruvlaad@gmail.com>
2020-12-25 20:58:47 +0530

    assignment code

---
+++ A7/bonus.py
@@ -0,0 +1,60 @@
+import VBMF
+import torch
+import torch.nn as nn
+import numpy as np
+from torch.autograd import Variable
+import tensorly as tl
+from tensorly.decomposition import parafac, partial_tucker
+
+
+def cp_decomposition(W: torch.Tensor, rank) -> nn.Module:
+    last, first, vertical, horizontal = parafac(W.detach().numpy(), rank=rank, init='svd')[1]
+    
+    conv1 = nn.Conv2d(in_channels=first.shape[0], out_channels=rank, kernel_size=1, bias=False)
+    conv2 = nn.Conv2d(in_channels=rank, out_channels=rank, kernel_size=(vertical.shape[0], 1), groups=rank, bias=False)
+    conv3 = nn.Conv2d(in_channels=rank, out_channels=rank, kernel_size=(1, horizontal.shape[0]), groups=rank, bias=False)
+    conv4 = nn.Conv2d(in_channels=rank, out_channels=last.shape[0], kernel_size=1, bias=False)
+
+    conv1.weight.data = torch.from_numpy(np.transpose(first)).unsqueeze(-1).unsqueeze(-1)
+    conv2.weight.data = torch.from_numpy(np.transpose(vertical)).unsqueeze(1).unsqueeze(-1)
+    conv3.weight.data = torch.from_numpy(np.transpose(horizontal)).unsqueeze(1).unsqueeze(1)
+    conv4.weight.data = torch.from_numpy(last).unsqueeze(-1).unsqueeze(-1)
+
+    return nn.Sequential(conv1, conv2, conv3, conv4)
+
+def determine_rank_for_cp_decomp(W: torch.Tensor, rank_range: int, input_tensor: torch.Tensor):
+    from tqdm import tqdm
+    from torch.nn.functional import conv2d
+
+    min_delta = np.infty
+    for rank in tqdm(range(*rank_range)):
+        decomp_conv = cp_decomposition(W, rank)
+        delta = (conv2d(input_tensor, W) - decomp_conv(input_tensor)).abs().max()
+        if delta < min_delta:
+            min_rank = rank
+    
+    print(f'Minimum rank required to maintain accuracy: {min_rank}')
+    return min_rank
+
+def estimate_ranks(W: torch.Tensor):
+    unfold_0 = tl.base.unfold(W, 0) 
+    unfold_1 = tl.base.unfold(W, 1)
+    _, diag_0, _, _ = VBMF.EVBMF(unfold_0)
+    _, diag_1, _, _ = VBMF.EVBMF(unfold_1)
+    ranks = [diag_0.shape[0], diag_1.shape[1]]
+    return ranks
+
+def tucker(W: torch.Tensor, ranks):
+    core, [last, first] = partial_tucker(W.detach().numpy(), modes=[0, 1], rank=ranks, init='svd')   
+    kernel_size = tuple(W.shape[2:])
+
+    # A pointwise convolution that reduces the channels from S to R3
+    conv1 = nn.Conv2d(in_channels=first.shape[0], out_channels=first.shape[1], kernel_size=1, bias=False)
+    conv2 = nn.Conv2d(in_channels=core.shape[1], out_channels=core.shape[0], kernel_size=kernel_size, bias=False)
+    conv3 = nn.Conv2d(in_channels=last.shape[1], out_channels=last.shape[0], kernel_size=1, bias=False)
+    
+    conv1.weight.data = torch.from_numpy(np.transpose(first)).unsqueeze(-1).unsqueeze(-1)
+    conv2.weight.data = torch.from_numpy(core)
+    conv3.weight.data = torch.from_numpy(last).unsqueeze(-1).unsqueeze(-1)
+
+    return nn.Sequential(conv1, conv2, conv3)
---
+++ A7/main.py
@@ -0,0 +1,102 @@
+import torch
+import numpy as np
+
+from torch.nn import Unfold
+from winograd import Winograd
+from fft_conv import fft_conv
+from torch.autograd import Variable
+from bonus import *
+
+CPU_RANK_CP_DECOMP = 74
+CPU_RANK_TUCKER = None
+
+def direct_conv(input_tensor: torch.Tensor, filter: torch.Tensor) -> torch.Tensor:
+    N, C1, H, W = input_tensor.shape
+    M, C2, R, S = filter.shape
+
+    assert C1 == C2, f'Input tensor channels ({C1}) do not match with filter channels ({C2})'
+    C = C1
+
+    output_tensor = Variable(torch.zeros(N, M, H - R + 1, W - S + 1, dtype=input_tensor.dtype), requires_grad=True)
+    
+    from tqdm import tqdm
+    pbar = tqdm(total=N*C*(H - R + 1)*(W - S + 1)*M*R*S)
+
+    for n in range(N):
+        for h in range(H - R + 1):
+            for w in range(W - S + 1):
+                for m in range(M):
+                    conv_out = 0.0
+                    for c in range(C):
+                        for r in range(R):
+                            for s in range(S):
+                                conv_out += filter[m, c, r, s] * input_tensor[n, c, h + r, w + s]
+                                pbar.update(1)
+                    output_tensor[n, m, h, w] = conv_out
+    
+    return output_tensor
+
+def im2col(input_tensor: torch.Tensor, filter: torch.Tensor) -> torch.Tensor:
+    N, C1, H, W = input_tensor.shape
+    M, C2, R, S = filter.shape
+
+    assert C1 == C2, f'Input tensor channels ({C1}) do not match with filter channels ({C2})'
+    C = C1
+
+    unfold = Unfold(kernel_size=(R, S))
+    im_as_col = unfold(input_tensor)
+    
+    out_as_col = im_as_col.transpose(1, 2).matmul(filter.view(filter.size(0), -1).t()).transpose(1, 2)
+    out = out_as_col.reshape(out_as_col.size(0), out_as_col.size(1), H - R + 1, W - S + 1).contiguous()
+    return out
+    
+def winograd(input_tensor: torch.Tensor, filter: torch.Tensor) -> torch.Tensor:
+    out = Winograd.forward(input_tensor, filter)
+    return out
+
+def fft(input_tensor: torch.Tensor, filter: torch.Tensor) -> torch.Tensor:
+    N, C1, H, W = input_tensor.shape
+    M, C2, R, S = filter.shape
+
+    assert C1 == C2, f'Input tensor channels ({C1}) do not match with filter channels ({C2})'
+    C = C1
+
+    out = fft_conv(input_tensor, filter)
+    return out
+
+def cp_decomp(input_tensor: torch.Tensor, filter: torch.Tensor) -> torch.Tensor:
+    # rank = determine_rank_for_cp_decomp(filter, rank_range=[20, 70], input_tensor=input_tensor) if rank is None else rank
+    rank = max(filter.shape) // 3
+    conv = cp_decomposition(filter, rank=rank)
+    return conv(input_tensor)
+
+def tucker_decomp(input_tensor: torch.Tensor, filter: torch.Tensor, determine_rank=False) -> torch.Tensor:
+    if determine_rank:
+        ranks = estimate_ranks(filter)
+        print(f'Ranks for given filter: {ranks}')
+    else:
+        ranks = CPU_RANK_CP_TUCKER
+    
+    conv = tucker(filter, ranks)
+    return conv(input_tensor)
+
+def verify_output():
+    from torch.nn.functional import conv2d
+
+    N, C, H, W = 8, 3, 224, 224
+    M, R, S = 16, 5, 5
+
+    weight = Variable(torch.rand(M, C, R, S, dtype=torch.float32), requires_grad=True)
+    input_tensor = torch.rand(N, C, H, W, dtype=torch.float32)
+
+    # out = im2col(input_tensor, weight)
+    # out = direct_conv(input_tensor, weight)
+    # out = winograd(input_tensor, weight)
+    # out = fft(input_tensor, weight)
+    # out = cp_decomp(input_tensor, weight, rank=CPU_RANK_CP_DECOMP)
+    out = tucker_decomp(input_tensor, weight, determine_rank=True)
+
+    print(f'Max discrepancy in results = {(conv2d(input_tensor, weight) - out).abs().max()}')
+
+if __name__ == "__main__":
+    verify_output()
